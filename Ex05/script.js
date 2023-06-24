e794bd4c03270271a0281641a1f75f9f1e6afe39
unknown <dedekaio@bol.com.br>
2023-06-23 21:02:55 -0300

    ex05

---
+++ Ex05/script.js
@@ -0,0 +1,92 @@
+let txtnumero = document.getElementById('txtnum');
+//let numero = Number(txtnumero.value); //Converte o número capturado -> para int
+
+let tabela = document.querySelector('select#tabela');
+
+let msg = document.getElementById('resultado');
+
+let valores = []; //Vetor que guarda os valores digitados
+
+let numTotal = 0;
+let maior = 0;
+let menor = 0;
+let total = 0;  
+let media = 0;
+
+
+//Valida se o número é entre 1--100
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true;
+    }else{
+        return false;
+    }
+}
+//Valida se o número já esta na lista
+function inLista(n,l){
+    if (l.indexOf(Number(n)) != -1) {
+        //Numero não está na lista
+        return true;
+    }else{
+        //Número já está na lista
+        return false;
+    }
+}
+
+
+
+function adicionar(){
+    //Validar Número:
+    if (isNumero(txtnumero.value) && !inLista(txtnumero.value, valores)){
+        let numero = Number(txtnumero.value); //Converter o valor do input -> para Int
+        //Adicionar o número no array
+        valores.push(numero);//Adiciona o numero na ultima posição do array
+
+        //Adicionar um <option> com o número adicionado
+        var opcao = document.createElement('option');
+        opcao.innerHTML =  `${numero}`;
+        tabela.appendChild(opcao);
+
+        //Contagem do total de números adicionados
+        numTotal++;//Sempre q um número for adicionado, incrementa +1 a variavel numTotal
+
+        //Soma de todos os números adicionados
+        total += numero;//Sempre q um número for adicionado, ele será somado dentro do total
+
+        msg.innerHTML = '';
+
+    }else{
+        window.alert('Valor digitado é inválido ou já está está na tabela!!!');
+    }
+
+}
+
+
+function finalizar(){
+    media = parseFloat(media);
+    media = (total/numTotal);//Calcular media
+
+    //Validar qual é o MAIOR e o MENOR número digitado
+    for (let cont in valores){
+        if (cont == 0){
+            maior = valores[cont]
+            menor = valores[cont]
+        }else{
+            if (valores[cont] > maior){//Se o valor do indice for > que MAIOR, ele se torna o maior
+                maior = valores[cont]
+            }else if(valores[cont] < menor){ //Se o valor do incice for < que MENOR, ele se torna o menor
+                menor = valores[cont]
+            }
+
+        }
+    }
+
+    //[1,2,3] -> numTotal = 3; maior = 3; menor = 1; total = 6; media = 6/3 = 2
+    msg.innerHTML += `No total foram adicionados ${numTotal} números a tabela <br>`;
+    msg.innerHTML += `O maior valor digitado foi ${maior} <br>`;
+    msg.innerHTML += `O menor valor digitado foi ${menor} <br>`;
+    msg.innerHTML += `A soma total de todos os números digitados, é igual a ${total} <br>`;
+    msg.innerHTML += `A média entre todos os números digitados é igual a ${media} <br>`;
+
+
+}
